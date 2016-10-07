84582519d3ae3bb9a8df04abc48bd5fe970c0c5e
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2016-10-07 15:07:34 +0100

    initial commit 2

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './components/TodoApp';
+
+$(document).foundation();
+require('style!css!sass!./styles/app.scss');
+
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TodoApp extends React.Component {
+  render() {
+    return <h1>TodoApp.jsx</h1>
+  }
+}
+
+export default TodoApp
