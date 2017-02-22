761bc06ae24491e7c7f94cf7568f9d43b18479ee
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2017-02-22 16:11:07 +0000

    Updated webpack config for heroku webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var webpack = require("webpack");
-var path = require("path");
+let webpack = require("webpack");
+let path = require("path");
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
@@ -14,6 +16,11 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
     })
   ],
   output: {
@@ -45,5 +52,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'eval-source-map'
+  devtool: process.env.NODE_ENV === 'production'?undefined:'source-map'
 };
